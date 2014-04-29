df0f71edf81fd5622aaeeebd10148e53e28cb197
ksouf <khaled.souf@gmail.com>
2014-04-29 22:33:20 +0200

    first example liquibase/flyway

---
+++ flyway-workshop-sample/src/main/resources/db/migration/V1.2__Add_nightclasses.sql
@@ -0,0 +1,3 @@
+insert into NIGHTCLASS (ID, TITLE) values (1, 'AngularJS decouverte');
+insert into NIGHTCLASS (ID, TITLE) values (2, 'AngularJS avancee');
+insert into NIGHTCLASS (ID, TITLE) values (3, 'Build Tools Continous Delivery decouverte');
