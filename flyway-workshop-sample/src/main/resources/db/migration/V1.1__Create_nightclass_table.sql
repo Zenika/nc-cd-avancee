768abd84db2f1d03af0e32afa7c5cb68abde7363
ksouf <khaled.souf@gmail.com>
2014-05-05 23:43:33 +0200

    deleting unused files

--- flyway-workshop-sample/src/main/resources/db/migration/V1.1__Create_nightclass_table.sql
+++
@@ -1,4 +0,0 @@
-create table NIGHTCLASS (
-    ID int not null,
-    TITLE varchar(100) not null
-);
--- flyway-workshop-sample/src/main/resources/db/migration/V1.2__Add_nightclasses.sql
+++
@@ -1,3 +0,0 @@
-insert into NIGHTCLASS (ID, TITLE) values (1, 'AngularJS decouverte');
-insert into NIGHTCLASS (ID, TITLE) values (2, 'AngularJS avancee');
-insert into NIGHTCLASS (ID, TITLE) values (3, 'Build Tools Continous Delivery decouverte');
